e61bf5b0920eb731f2186f6b6681875f0c9e3694
kduplex-droid <k.duplex16@gmail.com>
2026-03-09 11:27:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let clicks = 0;
+let clicks = localStorage.getItem("potClicks");
+
+if (clicks === null) {
+    clicks = 0;
+} else {
+    clicks = parseInt(clicks);
+}
+
+document.getElementById("counter").innerText = clicks;
 
 function addClick() {
-
     clicks++;
-
     document.getElementById("counter").innerText = clicks;
+    localStorage.setItem("potClicks", clicks);
+}
 
+function resetCount() {
+    clicks = 0;
+    document.getElementById("counter").innerText = clicks;
+    localStorage.setItem("potClicks", clicks);
 }
